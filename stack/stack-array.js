ac9fc3073d6e547ced5197c90d4cb4e19119a0b3
Serge Kovbasiuk <sergekovbasiuk@Serges-MacBook-Pro.local>
2020-01-06 16:24:47 +0800

    minor

--- stack/stack-array.js
+++ stack/stack-array.js
@@ -22,7 +22,7 @@ stack.push(1);
 stack.push(2);
 stack.push(3);
 console.log([...stack])
-console.dir(stack.pop());
-console.dir(stack.pop());
-console.dir(stack.pop());
-console.dir(stack.pop());
+console.log(stack.pop()); // 3
+console.log(stack.pop()); // 2
+console.log(stack.pop()); // 1
+console.log(stack.pop()); // undefined
--- stack/stack-linked-list.js
+++ stack/stack-linked-list.js
@@ -33,7 +33,7 @@ stack.push(2)
 stack.push(3)
 
 console.log([...stack])
-console.dir(stack.pop())
-console.dir(stack.pop())
-console.dir(stack.pop())
-console.dir(stack.pop())
+console.log(stack.pop()); // 3
+console.log(stack.pop()); // 2
+console.log(stack.pop()); // 1
+console.log(stack.pop()); // undefined
