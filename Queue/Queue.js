daee6d03b0e4a30b57a26f537a67dfdb3984e8fc
Serge Kovbasiuk <sergekovbasiuk@Serges-MacBook-Pro.local>
2020-06-16 08:36:17 +0800

    updated Queue

---
+++ Queue/Queue.js
@@ -0,0 +1,41 @@
+const createQueue = () => ({
+  head: null,
+  tail: null,
+  length: 0,
+  *[Symbol.iterator]() {
+    let node = this.head;
+
+    while (node) {
+      yield node.item;
+      node = node.next;
+    }
+  },
+  put(item) {
+    const node = { next: null, item };
+    const { tail } = this;
+    if (tail) {
+      tail.next = node;
+      this.tail = node;
+    } else {
+      this.head = node;
+      this.tail = node;
+    }
+    this.length++;
+  },
+  pick() {
+    const { head } = this;
+    if (!head) return null;
+    if (this.tail === head) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = head.next;
+    }
+    this.length--;
+    return head.item;
+  },
+});
+
+module.exports = {
+  createQueue,
+};
---
+++ Queue/Queue.test.js
@@ -0,0 +1,42 @@
+const { createQueue } = require('./Queue');
+
+it("Should create empty queue", () => {
+    const queue = createQueue();
+    expect(queue.length).toBe(0);
+    expect(queue.head).toBeNull();
+    expect(queue.tail).toBeNull();
+});
+
+it("Should put new elements to queue", () => {
+    const queue = createQueue();
+
+    expect(queue.head).toBeNull();
+
+    queue.put(1);
+    queue.put(2);
+
+    expect(queue.length).toBe(2);
+    expect(queue.head.item).toBe(1);
+    expect(queue.tail.item).toBe(2);
+});
+
+it("Should pick elements from queue", () => {
+    const queue = createQueue();
+
+    expect(queue.head).toBeNull();
+
+    queue.put(1);
+    queue.put(2);
+
+    expect(queue.pick()).toBe(1);
+    expect(queue.pick()).toBe(2);
+    expect(queue.length).toBe(0);
+});
+
+it("Should return null if pick from empty queue", () => {
+    const queue = createQueue();
+
+    expect(queue.head).toBeNull();
+    expect(queue.length).toBe(0);
+    expect(queue.pick()).toBeNull();
+});
