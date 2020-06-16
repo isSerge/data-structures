84bf61308293aa6a73441951a901a424e665bb1a
Serge Kovbasiuk <sergekovbasiuk@Serges-MacBook-Pro.local>
2020-06-16 21:42:30 +0800

    added HashTable

---
+++ HashTable/HashTable.js
@@ -0,0 +1,58 @@
+const DEFAULT_STORAGE_SIZE = 140;
+
+const defaultHashing = (key, max) => [...key].reduce((acc, x) => acc + x.charCodeAt(0), 0) % max;
+
+const createHashTable = (storageMaxSize = DEFAULT_STORAGE_SIZE, getHash = defaultHashing) => ({
+    storage: Array(storageMaxSize).fill([]),
+    map: Object.create(null),
+    size: 0,
+    set(key, value) {
+        const hash = getHash(key, storageMaxSize);
+        this.map[key] = hash;
+        const storageItem = this.storage[hash];
+        const node = storageItem.find(x => x.key === key);
+
+        if (!node) {
+            storageItem.push({ key, value });
+            this.size++;
+        } else {
+            node.value = value;
+        }
+    },
+    get(key) {
+        const hash = getHash(key, storageMaxSize);
+        const storageItem = this.storage[hash];
+        const node = storageItem.find(x => x.key === key);
+        return node && node.value;
+    },
+    has(key) {
+        return !!this.map[key];
+    },
+    delete(key) {
+        delete this.map[key];
+        const hash = getHash(key, storageMaxSize);
+        const storageItem = this.storage[hash];
+        const node = storageItem.find(x => x.key === key);
+
+        if (node) {
+            this.storage[hash] = storageItem.filter(x => x.key !== key);
+            return node;
+        }
+
+        return null;
+    },
+    clear() {
+        this.map = Object.create(null);
+        this.storage = Array(storageMaxSize).fill([]);
+        this.size = 0;
+    },
+    keys() {
+        return Object.keys(this.map);
+    },
+})
+
+module.exports = {
+    DEFAULT_STORAGE_SIZE,
+    defaultHashing,
+    createHashTable,
+}
---
+++ HashTable/HashTable.test.js
@@ -0,0 +1,111 @@
+const { createHashTable, DEFAULT_STORAGE_SIZE } = require('./HashTable')
+
+it("Should create hash table with default storage size", () => {
+    const table = createHashTable();
+
+    expect(table.size).toBe(0);
+    expect(table.storage.length).toBe(DEFAULT_STORAGE_SIZE);
+});
+
+it("Should create hash table with provided storage size", () => {
+    const NEW_STORAGE_SIZE = 100;
+    const table = createHashTable(NEW_STORAGE_SIZE);
+
+    expect(table.size).toBe(0);
+    expect(table.storage.length).toBe(NEW_STORAGE_SIZE);
+});
+
+
+it("Should set key and value", () => {
+    const table = createHashTable();
+
+    expect(table.size).toBe(0);
+
+    table.set('first', 1);
+    table.set('second', 2);
+
+    expect(table.size).toBe(2);
+    expect(table.get('first')).toBe(1);
+    expect(table.get('second')).toBe(2);
+});
+
+it("Should update value if key already exists", () => {
+    const table = createHashTable();
+
+    expect(table.size).toBe(0);
+
+    table.set('first', 1);
+    table.set('first', 2);
+
+    expect(table.size).toBe(1);
+
+    expect(table.get('first')).toBe(2);
+});
+
+it("Should return value for existing key", () => {
+    const table = createHashTable();
+
+    table.set('key', 2);
+
+    expect(table.get('key')).toBe(2);
+});
+
+it("Should return undefined if no key", () => {
+    const table = createHashTable();
+
+    expect(table.get('key')).toBeUndefined();
+});
+
+it("Should return true if value was set", () => {
+    const table = createHashTable();
+
+    table.set('key', 2);
+
+    expect(table.has('key')).toBe(true);
+});
+
+it("Should return false if has no value and key", () => {
+    const table = createHashTable();
+
+    expect(table.has('key')).toBe(false);
+});
+
+it("Should delete and return element by key", () => {
+    const table = createHashTable();
+
+    table.set('key', 2);
+
+    expect(table.delete('key')).toEqual({ key: 'key', value: 2 });
+});
+
+it("Should return null if delete non-existent element", () => {
+    const table = createHashTable();
+
+    expect(table.delete('key')).toBeNull();
+});
+
+it("Should clear table", () => {
+    const table = createHashTable();
+
+    expect(table.size).toBe(0);
+
+    table.set('first', 1);
+    table.set('second', 2);
+
+    expect(table.size).toBe(2);
+
+    table.clear();
+
+    expect(table.size).toBe(0);
+    expect(table.has('first')).toBe(false);
+    expect(table.has('second')).toBe(false);
+});
+
+it("Should return table keys", () => {
+    const table = createHashTable();
+
+    table.set('first', 1);
+    table.set('second', 2);
+
+    expect(table.keys()).toEqual(['first', 'second']);
+});
